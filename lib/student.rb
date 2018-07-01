303a1a7c9c82292736d5837e9f50ff6401958d6b
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:12:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
       SELECT * FROM students
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
