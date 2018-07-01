3ff10bde8696372090e5e157c71c38e8a222469b
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:18:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,9 +53,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10
     SQL
-
     i = number
-
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
