1431b15c75ed4a261bb25386b63f110463da1e5c
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:16:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
       SELECT * FROM students WHERE grade = 9
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row
   end
 
   def self.students_below_12th_grade
