faa367ca887136c26e95620e5d6e3e0de7200945
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:17:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,13 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
