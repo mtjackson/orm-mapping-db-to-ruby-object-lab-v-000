113607301d3f6b67ef2b1db87025025ed7174d3c
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:13:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
       SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name).map
+    DB[:conn].execute(sql, name).map do |row
   end
 
   def save
