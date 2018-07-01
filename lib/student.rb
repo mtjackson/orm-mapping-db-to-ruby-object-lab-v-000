458806b54e443b2a47bb104c838647583d5a6ab1
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:10:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,7 @@ class Student
   def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
+    new_student.name = row[1]
     # create a new Student object given a row from the database
   end
 
