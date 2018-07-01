4783ce22fca7b18fbf3ecad8de94f2e00d807c58
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:23:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
 
   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10
+      SELECT * FROM students WHERE grade = 10 LIMIT 2
     SQL
 
     DB[:conn].execute(sql).map do |row|
