e7a0440d5a4ec5ea79c387a93d5215e9bc747819
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:26:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,7 @@ class Student
 
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10 LIMIT 1
+      SELECT * FROM students WHERE grade = ?
     SQL
 
     DB[:conn].execute(sql).map do |row|
