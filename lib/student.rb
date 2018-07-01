507f7c865fbb229990b1394cd473c45d47be01e3
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 14:23:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ class Student
       SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, number).map do |row|
       self.new_from_db(row)
     end
   end
